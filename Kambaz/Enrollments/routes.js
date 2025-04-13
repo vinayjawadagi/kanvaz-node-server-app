7d892e53184e2db225cbac990aade74a0b865734
jawadagiv <jawadagi.v@northeastern.edu>
2025-04-12 20:27:01 -0400

    added changes

--- Kambaz/Enrollments/routes.js
+++ Kambaz/Enrollments/routes.js
@@ -1,41 +1,35 @@
 import Database from "../Database/index.js";
 
 export default function EnrollmentRoutes(app) {
-    
-    app.get("/api/courses", (req, res) => {
-        const { courses } = Database;
-        console.log("Fetching courses from database:", Database.courses);
-        res.json(courses);
-    });
 
-    app.post("/api/courses/:cid/enroll", (req, res) => {
-        console.log("Current user session:", req.session["currentUser"]);
-        const currentUser = req.session["currentUser"];
-        if (!currentUser) {
-            res.status(401).json({ message: "Not logged in" });
-            return;
-        }
-        const { cid } = req.params;
+    app.post('/api/courses/:cid/enroll', (req, res) => {
+        // console.log('Current user session:', req.session['currentUser']);
+        // const currentUser = req.session['currentUser'];
+        // if (!currentUser) {
+        //     res.status(401).json({ message: 'Not logged in' });
+        //     return;
+        // }
+
         const enrollment = {
             _id: new Date().getTime().toString(),
-            user: currentUser._id,
-            course: cid
+            ...req.body,
         };
         Database.enrollments.push(enrollment);
         res.json(enrollment);
     });
 
-    app.delete("/api/courses/:cid/enroll", (req, res) => {
-        const currentUser = req.session["currentUser"];
-        if (!currentUser) {
-            res.status(401).json({ message: "Not logged in" });
-            return;
-        }
+    app.delete('/api/courses/:cid/enroll', (req, res) => {
+        // const currentUser = req.session["currentUser"];
+        // if (!currentUser) {
+        //     res.status(401).json({ message: "Not logged in" });
+        //     return;
+        // }
         const { cid } = req.params;
+        const userid = req.body.userid;
         Database.enrollments = Database.enrollments.filter(
-            e => !(e.user === currentUser._id && e.course === cid)
+            (e) => !(e.user === userid && e.course === cid)
         );
-        res.json({ message: "Successfully unenrolled" });
+        res.json({ message: 'Successfully unenrolled' });
     });
 
     app.get("/api/users/:uid/enrollments", (req, res) => {
